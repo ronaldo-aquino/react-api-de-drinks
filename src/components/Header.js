7681c8593e0dee3f5e682174909f0b206d449c1a
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-13 13:45:25 -0300

    Criando o Header e o fomulário

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="bg-alert">
+            <h1>Receitas de Bebidas.</h1>
+        </header>
+    )
+}
+
+export default Header
