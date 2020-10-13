df7ed4d5b2c6e543d004e89ce5c06f1664e62d50
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-13 15:05:55 -0300

    Campo busca e categoria dinêmico

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,46 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
+    const { categorias } = useContext(CategoriasContext);
+    const [busca, setBusca] = useState({
+        nome: "",
+        categoria: "",
+    });
 
-    const { categorias } = useContext(CategoriasContext)
-    console.log(categorias)
+    // Função para ler conteúdo
+    const getDataReceitas = (e) => {
+        setBusca({
+            ...busca,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return (
         <>
-        <form className="col-12">
-            <fieldset className="text-center">
-                <legend>Busca de bebidas por categoria ou ingredientes.</legend>
-            </fieldset>
+            <form className="col-12">
+                <fieldset className="text-center">
+                    <legend>
+                        Busca de bebidas por categoria ou ingredientes.
+                    </legend>
+                </fieldset>
 
-            <div className="row mt-4">
-                <div className="col-md-4">
-                    <input
-                        name="nome"
-                        className="form-control"
-                        type="text"
-                        placeholder="Buscar por ingredientes."
-                    />
+                <div className="row mt-4">
+                    <div className="col-md-4">
+                        <input
+                            name="nome"
+                            className="form-control"
+                            type="text"
+                            placeholder="Buscar por ingredientes."
+                            onChange={getDataReceitas}
+                        />
+                    </div>
+
+                    <div className="col-md-4">
+                        <select name="categoria" className="form-control"  onChange={getDataReceitas}>
+                            <option value="">-- Seleciona categoria</option>
+                            {categorias.map((categoria) => (
+                                <option
+                                    value={categoria.strCategory}
+                                    key={categoria.strCategory}
+                                >
+                                    {categoria.strCategory}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="col-md-4">
+                        <input
+                            type="submit"
+                            className="btn btn-block btn-primary"
+                            value="Buscar Bebidas"
+                        />
+                    </div>
                 </div>
-
-                <div className="col-md-4">
-                    <select name="categoria" className="form-control">
-                        <option value="">-- Seleciona categoria</option>
-                        {
-                            categorias.map((categoria) => (
-                            <option value={categoria.strCategory} key={categoria.strCategory}>
-                                {categoria.strCategory}
-                            </option>
-                            ))
-                        }
-                    </select>
-                </div>
-
-            <div className="col-md-4">
-                <input type="submit" className="btn btn-block btn-primary" value="Buscar Bebidas"/>
-            </div>
-
-            </div>
-        </form>
+            </form>
         </>
     );
 };
