fc1a47a794fb29762707fab25781de224fbc6309
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-13 13:56:22 -0300

    Configurando o formula´rio para API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Formulario = () => {
     return (
-        <h1>Formulário Aqui.</h1>
-    )
-}
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca de bebidas por categoria ou ingredientes.</legend>
+            </fieldset>
 
-export default Formulario
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="nome"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por ingredientes."
+                    />
+                </div>
+
+                <div className="col-md-4">
+                    <select name="categoria" className="form-control">
+                        <option value="">-- Seleciona categoria</option>
+                    </select>
+                </div>
+
+            <div className="col-md-4">
+                <input type="submit" className="btn btn-block btn-primary" value="Buscar Bebidas"/>
+            </div>
+
+            </div>
+        </form>
+    );
+};
+
+export default Formulario;
