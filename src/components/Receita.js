92a82c599fb7f583a05927d0dd5aa829f3112c4e
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-15 14:02:27 -0300

    Configurando modal

--- src/components/Receita.js
+++ src/components/Receita.js
@@ -1,7 +1,45 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { ModalContext } from "../context/ModalContext";
+
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: "absolute",
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 const Receita = ({ receita }) => {
+    // Configuração do modal do material-ui
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     // Extrair os valores do context
     const { setIdReceita } = useContext(ModalContext);
 
@@ -16,11 +54,28 @@ const Receita = ({ receita }) => {
                 />
 
                 <div className="card-body">
-                    <button className="btn btn-block btn-primary" onClick={() => {
-                        setIdReceita(receita.idDrink)
-                    }}>
+                    <button
+                        className="btn btn-block btn-primary"
+                        onClick={() => {
+                            setIdReceita(receita.idDrink);
+                            handleOpen();
+                        }}
+                    >
                         Ver Receita
                     </button>
+
+                    <Modal 
+                        open={open} 
+                        onClose={() => {
+                            setIdReceita(null)
+                            handleClose()
+                        }}
+                        
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>A partir do modar</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
