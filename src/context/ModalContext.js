05a57d3e321d65fb8db39e7121cbad83b0406f33
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-15 13:29:46 -0300

    Consultando api sempre que clicar em uma bebida com id diferente

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import { useState } from 'react'
-import { createContext } from 'react'
+import React, { useEffect, createContext, useState } from "react";
+import axios from "axios";
 
 // Criar Context
-export const ModalContext = createContext()
+export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
-
     // Estado do provider
-    const [ idReceita, setIdReceita ] = useState(null)
+    const [idReceita, setIdReceita] = useState(null);
+    const [receitaPorId, setReceitaPorId] = useState({})
 
+    // Uma vez que tem uma receita com base no id, chamar a API
+    useEffect(() => {
+        const getReceita = async () => {
+            if (!idReceita) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceita}`;
+            const resultado = await axios(url);
+            setReceitaPorId(resultado.data.drinks[0]);
+        };
+
+        getReceita();
+    }, [idReceita]);
 
     return (
-        <ModalContext.Provider value={{setIdReceita}}>
+        <ModalContext.Provider value={{ setIdReceita }}>
             {props.children}
         </ModalContext.Provider>
     );
-}
+};
 
-export default ModalProvider
+export default ModalProvider;
